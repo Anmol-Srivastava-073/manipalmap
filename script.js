64d83c7c293f3d46de1ad854e7c9c7252580dcc6
ANMOL SRIVASTAVA <anmolsrivastava073@gmail.com>
2025-09-15 16:18:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,11 @@ window.addEventListener("scroll", () => {
     }
   }
 });
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registered:', reg))
+      .catch(err => console.log('Service Worker failed:', err));
+  }
+
+
